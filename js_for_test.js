0ec54f30b182c6e8d49e16ee9c5fde8228a7225e
dangew <dangew99@gmail.com>
2025-01-21 08:50:49 +0900

    File structure, zoom.js fixing

--- js_for_test.js
+++
@@ -1,2 +0,0 @@
-console.log("js");
-console.log("commit");
--- zoom.js
+++ zoom.js
@@ -1,23 +1,23 @@
-let clothes = document.querySelectorAll(".clothe");
+let clothes = document.querySelectorAll(".clothe-box");
 
 clothes.forEach((cloth) => {
-  cloth.addEventListener("mouseover", () => {
-    cloth.style.transform = "scale(1.2)";
-    cloth.style.zIndex = "1000";
+    cloth.addEventListener("mouseover", () => {
+        cloth.style.transform = "scale(1.2)";
+        cloth.style.zIndex = "1000";
 
-    // 나머지 이미지는 축소
-    clothes.forEach((other, otherIndex) => {
-      if (otherIndex !== index) {
-        other.style.transform = "scale(0.8)";
-        other.style.zIndex = ""; // 기본값으로 설정
-      }
+        // 나머지 이미지는 축소
+        clothes.forEach((other, otherIndex) => {
+            if (otherIndex !== index) {
+                other.style.transform = "scale(0.8)";
+                other.style.zIndex = ""; // 기본값으로 설정
+            }
+        });
     });
-  });
 
-  cloth.addEventListener("mouseout", () => {
-    clothes.forEach((reset) => {
-      cloth.style.transform = "scale(1)";
-      cloth.style.zIndex = ""; // 기본값으로 복원
+    cloth.addEventListener("mouseout", () => {
+        clothes.forEach((reset) => {
+            cloth.style.transform = "scale(1)";
+            cloth.style.zIndex = ""; // 기본값으로 복원
+        });
     });
-  });
 });
